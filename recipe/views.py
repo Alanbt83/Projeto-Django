2c17b526fcf396dfc89f4fa681f79832679e3b8a
Alanbt83 <allan-fbi@hotmail.com>
2022-05-18 19:36:08 -0300

    ok

--- recipe/views.py
+++ recipe/views.py
@@ -8,3 +8,7 @@ def home(request):
     return render(request, 'recipe/pages/home.html', context={
         'nome': 'Alan Barbosa Taborda',
     })
+
+#pasta template o jango ja reconhece automatico
+#especificar o caminho para evitar colisão de dados 
+# função render vai carregar o arquivo .html
