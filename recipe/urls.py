83c7d7f669c7c5b3b1a78ea258b63910f26cf3bb
Alanbt83 <allan-fbi@hotmail.com>
2022-05-19 16:37:08 -0300

    criando views

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,23 +1,10 @@
-"""projeto URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
-from django.urls import path, include
-
-from recipe.urls import home
+from django.urls import path
+from recipe.views import home, contato, sobre
 
 urlpatterns = [
     path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,6 +9,15 @@ def home(request):
     return render(request, 'recipe/pages/home.html', context={
         'nome': 'Alan Barbosa Taborda',
     })
+
+
+def contato(request):
+    return HttpResponse('Contatos')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
 
 #pasta template o jango ja reconhece automatico
 #especificar o caminho para evitar colisão de dados 
