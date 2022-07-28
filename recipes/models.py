dbf51983dc6dca0e85ff242bbb512d1d215933fc
Alanbt83 <allan-fbi@hotmail.com>
2022-07-28 17:44:13 -0300

    ok

--- recipes/models.py
+++ recipes/models.py
@@ -9,7 +9,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -23,10 +22,14 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+
+    def __str__(self):
+        return self.title
