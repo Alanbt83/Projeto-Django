b816dca816821994417a1e96d2b0199f61e83eea
Alanbt83 <allan-fbi@hotmail.com>
2022-08-03 14:19:35 -0300

    alan

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""projeto URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from tomlkit import document
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
